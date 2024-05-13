b6d7b1da0d96345e100fd762ba5a91d26a229529
Alexwilliam112 <alexwilliam112@gmail.com>
2024-05-13 17:25:59 +0700

    Plotly

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = 3002
 //config
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 app.use(favicon(path.join(__dirname, 'public', 'icons', 'logo.png')))
 
 //routing
