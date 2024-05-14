5321754dc6427c63e96e34f99f67bd3a0ab081db
Alexwilliam112 <alexwilliam112@gmail.com>
2024-05-14 21:25:25 +0700

    Adjusted router. removed edit button from market orders

--- routers/Dashboard.js
+++ routers/Dashboard.js
@@ -4,6 +4,7 @@ const router = require('express').Router()
 const Controller = require('../controllers/controller')
 
 router.get('/', Controller.renderDashboard)
-
+router.post('/buyorder', Controller.buyPost)
+router.post('/sellorder', Controller.sellPost)
 
 module.exports = router
