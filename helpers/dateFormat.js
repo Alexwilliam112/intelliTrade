2284c7b6ad9874a317667868ef448a31bd8815a8
Alexwilliam112 <alexwilliam112@gmail.com>
2024-05-21 17:56:40 +0700

    Fixed search bar at admin company manager

--- helpers/dateFormat.js
+++ helpers/dateFormat.js
@@ -1,11 +1,11 @@
 'use strict'
 
-    module.exports = {
-        dateFormatter: (date) => {
-            return new Date(date).toLocaleString('id-ID', {
-                year: 'numeric',
-                month: 'numeric',
-                day: 'numeric',
-            });
-        }
+module.exports = {
+    dateFormatter: (date) => {
+        return new Date(date).toLocaleString('id-ID', {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+        });
     }
+}
