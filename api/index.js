22e8e42cd0b5736c7582e3622f6f38824ee31146
Alexwilliam112 <alexwilliam112@gmail.com>
2024-05-23 19:46:18 +0700

    Vercel deployment trial 3

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const session = require('express-session')
 
 const app = express()
-const router = require('./routers/index')
+const router = require('../routers/index')
 const PORT = 3002
 require('dotenv').config()
 
